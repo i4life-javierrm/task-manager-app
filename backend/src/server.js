2dffcb6456ad1c91d95b3c0b519d0046424e5859
Javier Rodríguez Martínez <i4life.javierrm@gmail.com>
2025-10-06 13:50:02 +0200

    db hecha

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors()); 
 app.use(express.json()); // Permite recibir JSON en las solicitudes
 
+// Importar rutas de tareas 
+const taskRoutes = require('./routes/tasks.routes'); 
+app.use('/api', taskRoutes);
+
 app.get('/', (req, res) => { 
     res.send('¡Servidor funcionando!'); 
 });
