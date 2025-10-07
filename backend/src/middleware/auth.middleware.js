e7ae68ab0dc9913967084d25acd869f51370faed
Javier Rodríguez Martínez <i4life.javierrm@gmail.com>
2025-10-07 09:08:50 +0200

    back con autentificacion

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken'); 
+const SECRET_KEY = 'mi_secreto'; 
+module.exports = (req, res, next) => { 
+const token = req.headers.authorization?.split(' '); 
+if (!token) return res.status(401).json({ error: 'Acceso denegado' }); 
+try { 
+const decoded = jwt.verify(token, SECRET_KEY); 
+req.userId = decoded.userId; 
+next(); 
+} catch (error) { 
+res.status(401).json({ error: 'Token inválido' }); 
+} 
+};
